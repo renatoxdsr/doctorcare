85d120f32dfc5dc6ee6d1ba8fd4fd2fe6e4ccbf9
Renato <renatoxavier.rx@hotmail.com>
2022-05-04 05:32:17 -0300

    navigation

---
+++ script.js
@@ -0,0 +1,12 @@
+// String (textos)
+// Number (número)
+
+
+function onScroll(){
+    if(scrollY > 0 ){
+        navagation.classList.add('scroll')
+    } else {
+        navagation.classList.remove('scroll')
+    }
+
+}
