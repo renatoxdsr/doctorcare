2fea05b49180b031e154ba61dd01e02a9fc6640e
Renato <renatoxavier.rx@hotmail.com>
2022-05-07 14:00:48 -0300

    restructuring javascript

--- script.js
+++ script.js
@@ -7,6 +7,38 @@ onScroll()
 function onScroll(){
     showNavOnScroll()
     showBackToTopButtonOnScroll()
+    activateMenuatCurrentSection(home)
+    activateMenuatCurrentSection(services)
+    activateMenuatCurrentSection(about)
+    activateMenuatCurrentSection(contact)
+    /* activateMenuatCurrentSection(depoimentos) */
+}
+
+function activateMenuatCurrentSection(section){
+    const targetLine = scrollY + innerHeight / 2
+
+
+    //verificar se a seção passou da linha
+    //quais dados necessitarei?
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionTopReachorPassedTargetLine = targetLine >=sectionTop 
+
+    const sectionEndsAt = sectionTop + sectionHeight
+    const sectionEndPassedTargetLine = sectionEndsAt <= targetLine
+    
+
+    //limites das seção
+
+    const sectionBounderies = sectionTopReachorPassedTargetLine && !sectionEndPassedTargetLine
+
+    const sectionId = section.getAttibute('id')
+    const menuElement = document.querySelector(`.menu a [href*=${sectionId}]`)
+    menuElement.classList.remove('active')
+   
+    if(sectionBounderies){
+            menuElement.classList.add('active')
+    }
 }
 
 function showNavOnScroll(){
@@ -51,3 +83,10 @@ ScrollReveal({
 #about,
 #about header,
 #about .content`);
+
+
+function sayMyName(name){
+    console.log(name)
+}
+
+sayMyName('DESTENYchild')
